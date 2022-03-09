46a3c413052cc17d5d11dadab79320bb3f0611e5
kaotim710 <kaotim710@gmail.com>
2022-03-09 19:53:01 +0800

    forth

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -144,7 +144,6 @@ const VoxelDog = () =>{
                     mt = "calc(0px - var(--spinner-size))"
                     />
             )}
-                Dog!!!
                 </Box>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,19 @@ import {Container,
         Button,
         Box,
         Heading,
-        Link,
-        useColorModeValue
+        useColorModeValue,
+        chakra
 } from '@chakra-ui/react'
 import {ChevronRightIcon} from '@chakra-ui/icons'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import {BioSection, BioYear } from '../components/bio'
+import Image from 'next/image'
+
+const ProfileImage = chakra(Image,{
+  shouldForwardProp: prop =>['width', 'height','src','alt'].includes(prop)
+})
 
 const Page = () => {
     return(
@@ -22,15 +27,15 @@ const Page = () => {
             p={3} 
             mb={6} 
             textAlign="center">
-                Hello! I am Judy shake it from Taiwan
+                Nice to meet you! I am Tim.
             </Box>
 
             <Box display={{ md: 'flex'}}>
                 <Box flexGrow={1}>
                     <Heading as="h2" variant="page-title">
-                    Tim Kao
+                    Lai-ting, Kao &nbsp;|&nbsp; Tim
                     </Heading>
-        <p>Billionaire and Playboy</p>
+        <p>Engineer/Software developer/Photographer</p>
                 </Box>
              <Box 
                   flexShrink={0}
@@ -39,42 +44,52 @@ const Page = () => {
                   textAlign="center"
                 >
                <Box
-                borderColor ="whiteAlpha.800" 
-                borderWidth={2} 
-                borderStyle="solid" 
-                maxWidth = "150px" 
-                display = "inline-block" 
-                borderRadius="full" 
-                src="/images/Tim.png" 
-                alt="Profile Image"
+                borderColor="whiteAlpha.800"
+                borderWidth={2}
+                borderStyle="solid"
+                w="100px"
+                h="100px"
+                display="inline-block"
+                borderRadius="full"
+                overflow='hidden'
+              >
+                <ProfileImage
+                  src="/images/Tim.png"
+                  alt="Profile image"
+                  borderRadius='full'
+                  width="100%"
+                  height="100%"
                 />  
              </Box>
             </Box>
+            </Box>
 
-        <Section delay = {0.1}>
+        <Section delay = {0.1} >
             <Heading as="h3" variant="section-title">
-             Work
+            Work &amp; Academic Background<p/>
             </Heading>
-            <Paragraph>this is a book. a website call{' '}
-            <NextLink href="/works/inkdrop">
-                <Link>Inkdrop</Link>
-                 </NextLink>
+            <Paragraph>
+            &nbsp;
+            <p/>Field Service Engineer - Lam Research<p/>
+            Mandatory Army - Taipei City Government<p/> 
+            BSc - National Taipei University of Technology(NTUT), Taipei<p/>
+            Ex. Student - Shibaura Institute of Technology(SIT), Tokyo
                 </Paragraph>
-            <Box align="center" my={4}>
+            <Box align="center" my={8}>
                 <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon/>} colorScheme="teal">
-            My portfolio
+            <Button rightIcon={<ChevronRightIcon/>} colorScheme="yellow">
+            Find out more!
             </Button>
             </NextLink>
             </Box>
         </Section>
       <Section delay={0.2}>
         <Heading as="h3" variant="section-titile">
-            Bio
+        Bio
         </Heading>
       <BioSection>
         <BioYear>1995 </BioYear>
-        Born in Taipei, Taiwan. 
+        Born in Taipei, Taiwan.
       </BioSection>
       <BioSection>
         <BioYear>2018 </BioYear>
